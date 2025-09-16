8ef58463621ec7afab66bc576f525451ac7c3b06
ranjeet <ranjeet.kushwaha98@gmail.com>
2025-09-16 22:20:40 +0530

    rg2

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg4" {
+  name     = "rg4"
+  location = "West Europe"
+}
