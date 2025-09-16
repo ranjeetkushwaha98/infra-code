c8e91e07127a59408448c43a08596b9a690dd77f
ranjeetkushwaha98 <ranjeet.kushwaha98@gmail.com>
2025-09-16 21:51:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,3 @@ resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
   location = "West Europe"
 }
-
-resource "azurerm_resource_group" "rg2" {
-  name     = "rg2"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "rg3" {
-  name     = "rg3"
-  location = "West Europe"
-}
